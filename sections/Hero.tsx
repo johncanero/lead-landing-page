b0c605eeccab6f2157311b3d7cce261872642173
John Canero <johncanero.official@gmail.com>
2022-12-26 22:04:49 +0800

    Fix content put text above image with relative and absolute feature

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,21 +1,39 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <div>
-      <div className='flex mt-16'>
+      <div className='flex mt-14'>
 
         {/* Column 1 */}
-        <div>
-          <div>
-            <h1 className='max-w-xl text-6xl font-medium tracking-wide font-Montserrat '>A <span className='font-extrabold'>super solution</span> for your <span className='font-extrabold'>business.</span></h1>
+        <div className='flex'>
+          {/* Column 1.1 */}
+          <div className='relative'>
+            <div className='absolute'>
+              <h1 className='max-w-2xl font-medium text-7xl mt-28 font-Montserrat'>A <span className='font-extrabold'>super solution</span> for your <span className='font-extrabold'>business.</span></h1>
 
-            <p className='max-w-sm font-medium tracking-wider mt-14 font-Montserrat text-neutral-700 dark:text-neutral-500'>Our marketing team and sales automations <br></br>help you scale your outreach to get <br></br> more leads for your company.</p>
+              <p className='max-w-sm font-medium tracking-wider mt-14 font-Montserrat text-neutral-700 dark:text-neutral-500'>Our marketing team and sales automations <br></br>help you scale your outreach to get <br></br> more leads for your company.</p>
+
+              <button className="px-6 py-3 mt-16 mr-4 font-semibold text-white bg-transparent bg-orange-500 rounded hover:bg-orange-400 hover:text-black font-Montserrat">
+                Request Beta Access
+              </button>
+            </div>
+
+            <div className='ml-96'>
+              <Image
+                src="/images/hero/orangeLeadPortrait2.jpg"
+                width={370}
+                height={370}
+                alt="orangeLead Portrait"
+                className="ml-20 rounded-xl"
+              />
+            </div>
           </div>
 
-          <button className="px-6 py-3 mt-16 mr-4 font-semibold text-white bg-transparent bg-orange-500 rounded hover:bg-orange-400 hover:text-black font-Montserrat">
-            Request Beta Access
-          </button>
+
+  
+
         </div>
 
         {/* Column 2 */}
