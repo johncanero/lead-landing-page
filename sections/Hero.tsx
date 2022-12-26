77fd6e55ad557241f81e3042c2a5c2eec635fea8
John Canero <johncanero.official@gmail.com>
2022-12-26 22:21:22 +0800

    Add Image Content and Curve Line above Heading Title

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -4,14 +4,32 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <div>
-      <div className='flex mt-14'>
+      <div className='flex mt-12'>
 
         {/* Column 1 */}
         <div className='flex'>
           {/* Column 1.1 */}
           <div className='relative'>
-            <div className='absolute'>
-              <h1 className='max-w-2xl font-medium text-7xl mt-28 font-Montserrat'>A <span className='font-extrabold'>super solution</span> for your <span className='font-extrabold'>business.</span></h1>
+            <div className='absolute mt-14'>
+              {/* Light Curve Line */}
+              <Image
+                src="/images/hero/patternCurveLine.png"
+                width={300}
+                height={300}
+                alt="orangeLead Portrait"
+                className="dark:hidden rounded-xl ml-80"
+              />
+
+              {/* Light Curve Line */}
+              <Image
+                src="/images/hero/patternCurveLineWhite.png"
+                width={300}
+                height={300}
+                alt="orangeLead Portrait"
+                className="hidden dark:block rounded-xl ml-80"
+              />
+
+              <h1 className='max-w-2xl mt-1 font-medium text-7xl font-Montserrat'>A <span className='font-extrabold'>super solution</span> for your <span className='font-extrabold'>business.</span></h1>
 
               <p className='max-w-sm font-medium tracking-wider mt-14 font-Montserrat text-neutral-700 dark:text-neutral-500'>Our marketing team and sales automations <br></br>help you scale your outreach to get <br></br> more leads for your company.</p>
 
@@ -21,6 +39,8 @@ const Hero = () => {
             </div>
 
             <div className='ml-96'>
+
+
               <Image
                 src="/images/hero/orangeLeadPortrait2.jpg"
                 width={370}
@@ -32,7 +52,7 @@ const Hero = () => {
           </div>
 
 
-  
+
 
         </div>
 
