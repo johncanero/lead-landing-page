5019ffc56877d597118723eb0cddaebef6813613
John Canero <johncanero.official@gmail.com>
2022-12-27 19:44:39 +0800

    Add Lists of Digital Marketing Services from Chat GPT

--- pages/digital-marketing-services/index.tsx
+++ pages/digital-marketing-services/index.tsx
@@ -6,6 +6,69 @@ const index = () => {
       <h1 className="mt-6 text-2xl font-bold text-center text-neutral-600 lg:text-left md:text-3xl font-Montserrat dark:text-orange-500">
         Digital Marketing Services
       </h1>
+
+      {/* Digital Marketing Services */}
+      <div className="mx-8 mt-4 mb-12 md:mx-16 lg:mt-2 lg:mx-0 md:max-w-2xl lg:max-w-4xl font-Montserrat">
+        <p className="text-justify text-md">
+          1. Search engine optimization (SEO): optimizing website content and structure to improve organic search engine rankings and visibility.
+        </p>
+        <p className="text-justify text-md">
+          2. Pay-per-click (PPC) advertising: using platforms like Google AdWords to place ads and pay for clicks on those ads.
+        </p>
+
+        <p className="text-justify text-md">
+          3. Social media marketing: using social media platforms to promote products or services and engage with customers.
+        </p>
+
+        <p className="text-justify text-md">
+          5. Content marketing: creating and distributing valuable, relevant, and consistent content to attract and retain a clearly defined audience, ultimately leading to profitable customer action.
+        </p>
+        <p className="text-justify text-md">
+          6. Influencer marketing: partnering with individuals or organizations that have a large following on social media or other platforms to promote products or services.
+        </p>
+
+        <p className="text-justify text-md">
+          7. Affiliate marketing: earning a commission by promoting other people's or companies' products.
+        </p>
+
+        <p className="text-justify text-md">
+          8. Video marketing: using video to promote products or services, either on platforms like YouTube or through video ads on other websites.
+        </p>
+
+
+        <p className="text-justify text-md">
+          9. Mobile marketing: targeting and reaching customers through mobile devices, such as smartphones and tablets.
+        </p>
+
+        <p className="text-justify text-md">
+          10. Marketing automation: using software to automate marketing tasks, such as email campaigns and social media posts.
+        </p>
+
+        <p className="text-justify text-md">
+          11. Local SEO: optimizing a website for local search results, such as by including the location and phone number on the website and encouraging customer reviews.
+        </p>
+
+        <p className="text-justify text-md">
+          12. Voice search optimization: optimizing content for voice search queries, which are often more conversational and longer-tail than typed searches.
+        </p>
+
+        <p className="text-justify text-md">
+          13. Online reputation management: monitoring and improving a company's online reputation, including responding to customer reviews and managing the spread of negative information.
+        </p>
+
+        <p className="text-justify text-md">
+          14. Conversion rate optimization (CRO): optimizing a website to increase the percentage of visitors who take a desired action, such as making a purchase or filling out a form.
+        </p>
+        <p className="text-justify text-md">
+          15. Web design and development: creating and maintaining a professional and functional website.
+        </p>
+
+        {/* Chat GPT */}
+        <p className="mt-4 text-xs font-ShareTechMono md:text-sm dark:text-orange-500">
+          From ChatGPT: Optimizing Language Model for Dialogue
+        </p>
+
+      </div>
     </div>
   );
 };
