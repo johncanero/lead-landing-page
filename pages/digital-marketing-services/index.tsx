dce4efac816c5c7968f7041880e0044dc40c7b3a
John Canero <johncanero.official@gmail.com>
2022-12-27 19:38:41 +0800

    Change Color Text Heading of Pages

--- pages/digital-marketing-services/index.tsx
+++ pages/digital-marketing-services/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const index = () => {
   return (
     <div>
-      <h1 className="mt-6 text-2xl font-bold text-center lg:text-left md:text-3xl font-Montserrat dark:text-orange-500">
+      <h1 className="mt-6 text-2xl font-bold text-center text-neutral-600 lg:text-left md:text-3xl font-Montserrat dark:text-orange-500">
         Digital Marketing Services
       </h1>
     </div>
--- pages/get-a-quote/index.tsx
+++ pages/get-a-quote/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const index = () => {
   return (
     <div>
-      <h1 className="mt-6 text-2xl font-bold text-center lg:text-left md:text-3xl font-Montserrat dark:text-orange-500">
+      <h1 className="mt-6 text-2xl font-bold text-center lg:text-left md:text-3xl font-Montserrat text-neutral-600 dark:text-orange-500">
         Get A Quote
       </h1>
     </div>
