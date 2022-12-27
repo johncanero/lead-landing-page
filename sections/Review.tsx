71033870e4abfc2bc57775746065c7a2434038bc
John Canero <johncanero.official@gmail.com>
2022-12-28 00:34:58 +0800

    Example Content for Review

---
+++ sections/Review.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Review = () => {
+  return (
+    <div>
+      <div className='mt-12 bg-orange-300'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, animi. Sint et suscipit quod accusantium? Nemo suscipit dolore debitis, ullam minus ducimus! Vitae doloribus ipsa ipsum tempore, pariatur id voluptatibus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, animi. Sint et suscipit quod accusantium? Nemo suscipit dolore debitis, ullam minus ducimus! Vitae doloribus ipsa ipsum tempore, pariatur id voluptatibus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, animi. Sint et suscipit quod accusantium? Nemo suscipit dolore debitis, ullam minus ducimus! Vitae doloribus ipsa ipsum tempore, pariatur id voluptatibus?
+      </div>
+
+    </div>
+  )
+}
+
+export default Review
