91c8859a59d8b5ed233807df14b05e20cd743caf
John Canero <johncanero.official@gmail.com>
2022-12-28 11:39:57 +0800

    Fix Content Colum Mobile and Tablet

--- sections/Review.tsx
+++ sections/Review.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Review = () => {
   return (
     <div>
-      <div className='flex justify-center mx-32 mt-16 rounded-2xl bg-neutral-800'>
+      <div className='justify-center mt-16 md:pb-6 lg:pb-0 lg:flex lg:mx-36 md:mx-auto rounded-2xl bg-neutral-800'>
         {/* Column 1 */}
         <div className="relative -mt-10">
           <Image
@@ -12,25 +12,26 @@ const Review = () => {
             width={290}
             height={290}
             alt="people"
-            className="rounded-xl"
+            className="w-56 mx-auto rounded-xl lg:w-72"
           />
         </div>
 
         {/* Column 2 */}
-        <div className='flex mt-20 font-Montserrat'>
-          {/* Column 2.1 */}
-          <div>
-            <Image
-              src="/images/review/patternCurveDiagonalLine.png"
-              width={60}
-              height={60}
-              alt="Curve Diagonal Line"
-              className="mt-16 mx-14"
-            />
-          </div>
+        <div>
+          <Image
+            src="/images/review/patternCurveDiagonalLine.png"
+            width={60}
+            height={60}
+            alt="Curve Diagonal Line"
+            className="mx-auto mt-10 lg:mt-32 lg:mx-14"
+          />
+        </div>
 
-          {/* Column 2.2 */}
-          <div className='w-80'>
+        {/* Column 3 */}
+        <div className='mt-10 lg:mt-20 font-Montserrat'>
+
+          {/* Column 3.1 */}
+          <div className='mx-16 text-center lg:mx-0 lg:w-80'>
             <h2 className='text-4xl font-medium text-white'>It just <span className='font-bold '>works.</span></h2>
             <p className='mt-10 text-lg text-zinc-400'>"I really like how it is an all-in-one solution that handle many of the
               tasks that you would normally need seperate tools to do the same job. This thing is a miracle worker."</p>
@@ -41,7 +42,7 @@ const Review = () => {
           </div>
         </div>
       </div>
-      
+
     </div>
   )
 }
