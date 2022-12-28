60040256a25ba1d28a79ba9584b18da5b9c298fd
John Canero <johncanero.official@gmail.com>
2022-12-28 09:33:46 +0800

    Add Curve Line Content

--- sections/Review.tsx
+++ sections/Review.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Review = () => {
   return (
     <div>
-      <div className='flex justify-center mx-32 mt-16 rounded-2xl bg-zinc-800 gap-44'>
+      <div className='flex justify-center mx-32 mt-16 rounded-2xl bg-neutral-800'>
         {/* Column 1 */}
         <div>
           <Image
@@ -17,13 +17,27 @@ const Review = () => {
         </div>
 
         {/* Column 2 */}
-        <div className='mt-20 w-80 font-Montserrat'>
-          <h2 className='text-4xl font-medium text-white'>It just <span className='font-bold '>works.</span></h2>
-          <p className='mt-10 text-lg text-zinc-400'>"I really like how it is an all-in-one solution that handle many of the
-            tasks that you would normally need seperate tools to do the same job. This thing is a miracle worker."</p>
-          <div className='mt-10'>
-            <p className='font-bold text-white'>JEREMY ROBINSON</p>
-            <p className='font-medium text-zinc-400 '>CMO, FYLO</p>
+        <div className='flex mt-20 font-Montserrat'>
+          {/* Column 2.1 */}
+          <div>
+            <Image
+              src="/images/review/patternCurveDiagonalLine.png"
+              width={60}
+              height={60}
+              alt="Curve Diagonal Line"
+              className="mt-16 mx-14"
+            />
+          </div>
+
+          {/* Column 2.2 */}
+          <div className='w-80'>
+            <h2 className='text-4xl font-medium text-white'>It just <span className='font-bold '>works.</span></h2>
+            <p className='mt-10 text-lg text-zinc-400'>"I really like how it is an all-in-one solution that handle many of the
+              tasks that you would normally need seperate tools to do the same job. This thing is a miracle worker."</p>
+            <div className='mt-10'>
+              <p className='font-bold text-white'>JEREMY ROBINSON</p>
+              <p className='font-medium text-zinc-400 '>CMO, FYLO</p>
+            </div>
           </div>
         </div>
       </div>
