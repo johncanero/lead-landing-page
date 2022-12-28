20d6b7ab833a56b8b873af292350da7e1df5da54
John Canero <johncanero.official@gmail.com>
2022-12-28 16:42:10 +0800

    Add Image in Digital Marketing Services: Logo

--- pages/digital-marketing-services/index.tsx
+++ pages/digital-marketing-services/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const index = () => {
   return (
@@ -7,70 +8,82 @@ const index = () => {
         Digital Marketing Services
       </h1>
 
-      {/* Digital Marketing Services */}
-      <div className="mx-8 mt-4 mb-12 md:mx-16 lg:mt-2 lg:mx-0 md:max-w-2xl lg:max-w-4xl font-Montserrat">
-        <p className="text-justify text-md">
-          1. <span className="font-extrabold">Search engine optimization (SEO)</span>: optimizing website content and structure to improve organic search engine rankings and visibility.
-        </p>
-        <p className="text-justify text-md">
-          2. <span className="font-extrabold">Pay-per-click (PPC) advertising</span>: using platforms like Google AdWords to place ads and pay for clicks on those ads.
-        </p>
 
-        <p className="text-justify text-md">
-          3. <span className="font-extrabold">Social media marketing</span>: using social media platforms to promote products or services and engage with customers.
-        </p>
-        <p className="text-justify text-md">
-          4. <span className="font-extrabold">Email marketing</span>: sending promotional or informational emails to a list of subscribers.
-        </p>
+      <div className="flex">
+        {/* Digital Marketing Services */}
+        <div className="mx-8 mt-4 mb-12 md:mx-16 lg:mt-2 lg:mx-0 md:max-w-2xl lg:max-w-4xl font-Montserrat">
+          <p className="text-justify text-md">
+            1. <span className="font-extrabold">Search engine optimization (SEO)</span>: optimizing website content and structure to improve organic search engine rankings and visibility.
+          </p>
+          <p className="text-justify text-md">
+            2. <span className="font-extrabold">Pay-per-click (PPC) advertising</span>: using platforms like Google AdWords to place ads and pay for clicks on those ads.
+          </p>
 
-        <p className="text-justify text-md">
-          5. <span className="font-extrabold">Content marketing</span>: creating and distributing valuable, relevant, and consistent content to attract and retain a clearly defined audience, ultimately leading to profitable customer action.
-        </p>
-        <p className="text-justify text-md">
-          6. <span className="font-extrabold">Influencer marketing</span>: partnering with individuals or organizations that have a large following on social media or other platforms to promote products or services.
-        </p>
+          <p className="text-justify text-md">
+            3. <span className="font-extrabold">Social media marketing</span>: using social media platforms to promote products or services and engage with customers.
+          </p>
+          <p className="text-justify text-md">
+            4. <span className="font-extrabold">Email marketing</span>: sending promotional or informational emails to a list of subscribers.
+          </p>
 
-        <p className="text-justify text-md">
-          7. <span className="font-extrabold">Affiliate marketing</span>: earning a commission by promoting other people's or companies' products.
-        </p>
+          <p className="text-justify text-md">
+            5. <span className="font-extrabold">Content marketing</span>: creating and distributing valuable, relevant, and consistent content to attract and retain a clearly defined audience, ultimately leading to profitable customer action.
+          </p>
+          <p className="text-justify text-md">
+            6. <span className="font-extrabold">Influencer marketing</span>: partnering with individuals or organizations that have a large following on social media or other platforms to promote products or services.
+          </p>
 
-        <p className="text-justify text-md">
-          8. <span className="font-extrabold">Video marketing</span>: using video to promote products or services, either on platforms like YouTube or through video ads on other websites.
-        </p>
+          <p className="text-justify text-md">
+            7. <span className="font-extrabold">Affiliate marketing</span>: earning a commission by promoting other people's or companies' products.
+          </p>
+
+          <p className="text-justify text-md">
+            8. <span className="font-extrabold">Video marketing</span>: using video to promote products or services, either on platforms like YouTube or through video ads on other websites.
+          </p>
 
 
-        <p className="text-justify text-md">
-          9. <span className="font-extrabold">Mobile marketing</span>: targeting and reaching customers through mobile devices, such as smartphones and tablets.
-        </p>
+          <p className="text-justify text-md">
+            9. <span className="font-extrabold">Mobile marketing</span>: targeting and reaching customers through mobile devices, such as smartphones and tablets.
+          </p>
 
-        <p className="text-justify text-md">
-          10. <span className="font-extrabold">Marketing automation</span>: using software to automate marketing tasks, such as email campaigns and social media posts.
-        </p>
+          <p className="text-justify text-md">
+            10. <span className="font-extrabold">Marketing automation</span>: using software to automate marketing tasks, such as email campaigns and social media posts.
+          </p>
 
-        <p className="text-justify text-md">
-          11. <span className="font-extrabold ">Local SEO</span>: optimizing a website for local search results, such as by including the location and phone number on the website and encouraging customer reviews.
-        </p>
+          <p className="text-justify text-md">
+            11. <span className="font-extrabold ">Local SEO</span>: optimizing a website for local search results, such as by including the location and phone number on the website and encouraging customer reviews.
+          </p>
 
-        <p className="text-justify text-md">
-          12. <span className="font-extrabold">Voice search optimization</span>: optimizing content for voice search queries, which are often more conversational and longer-tail than typed searches.
-        </p>
+          <p className="text-justify text-md">
+            12. <span className="font-extrabold">Voice search optimization</span>: optimizing content for voice search queries, which are often more conversational and longer-tail than typed searches.
+          </p>
 
-        <p className="text-justify text-md">
-          13. <span className="font-extrabold">Online reputation management</span>: monitoring and improving a company's online reputation, including responding to customer reviews and managing the spread of negative information.
-        </p>
+          <p className="text-justify text-md">
+            13. <span className="font-extrabold">Online reputation management</span>: monitoring and improving a company's online reputation, including responding to customer reviews and managing the spread of negative information.
+          </p>
 
-        <p className="text-justify text-md">
-          14. <span className="font-extrabold ">Conversion rate optimization (CRO)</span>: optimizing a website to increase the percentage of visitors who take a desired action, such as making a purchase or filling out a form.
-        </p>
-        <p className="text-justify text-md">
-          15. <span className="font-extrabold">Web design and development</span>: creating and maintaining a professional and functional website.
-        </p>
+          <p className="text-justify text-md">
+            14. <span className="font-extrabold ">Conversion rate optimization (CRO)</span>: optimizing a website to increase the percentage of visitors who take a desired action, such as making a purchase or filling out a form.
+          </p>
+          <p className="text-justify text-md">
+            15. <span className="font-extrabold">Web design and development</span>: creating and maintaining a professional and functional website.
+          </p>
 
-        {/* Chat GPT */}
-        <p className="mt-4 text-xs font-ShareTechMono md:text-sm dark:text-orange-500">
-          From ChatGPT: Optimizing Language Model for Dialogue
-        </p>
+          {/* Chat GPT */}
+          <p className="mt-4 text-xs font-ShareTechMono md:text-sm dark:text-orange-500">
+            From ChatGPT: Optimizing Language Model for Dialogue
+          </p>
+        </div>
 
+        <div className="pl-12">
+          <Image
+            src="/images/leadIcon2.png"
+            width={350}
+            height={350}
+            alt="aLogo"
+            className=""
+          />
+        </div>
       </div>
     </div>
   );
