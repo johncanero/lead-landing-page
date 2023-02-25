f154dddd6125c4f15a6d0e96716d49afae4c0d03
John Canero <johncanero.official@gmail.com>
2023-02-25 13:06:05 +0800

    Adjust Padding of Pages Links

--- pages/digital-marketing-services/index.tsx
+++ pages/digital-marketing-services/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const index = () => {
   return (
-    <div className="mb-32 md:mb-0">
+    <div className="mb-32 md:mb-0 lg:px-32">
       <h1 className="mt-6 text-2xl font-bold text-center text-neutral-600 lg:text-left md:text-2xl font-Montserrat dark:text-neutral-500">
         Digital Marketing Services
       </h1>
--- pages/get-a-quote/index.tsx
+++ pages/get-a-quote/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const index = () => {
   return (
-    <div className="mb-32 md:mb-0">
+    <div className="mb-32 md:mb-0 lg:px-32">
       <h1 className="mt-6 text-2xl font-bold text-center lg:text-left md:text-2xl font-Montserrat text-neutral-600 dark:text-neutral-300">
         Get A Quote
       </h1>
