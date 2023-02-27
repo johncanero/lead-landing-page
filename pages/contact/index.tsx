4658cf506f5cbf66dc0865380afe1755956c9468
John Canero <johncanero.official@gmail.com>
2023-02-27 09:47:35 +0800

    Text Center Contact Heading

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const index = () => {
   return (
     <div className="mb-32 md:mb-0 lg:px-32">
-      <h1 className="mt-6 text-2xl font-bold text-center lg:text-left md:text-2xl font-Montserrat text-neutral-600 dark:text-neutral-500">
+      <h1 className="mt-6 text-2xl font-bold text-center mb-3 text-neutral-600
+       md:text-2xl font-Montserrat dark:text-neutral-300">
         Contact
       </h1>
     </div>
